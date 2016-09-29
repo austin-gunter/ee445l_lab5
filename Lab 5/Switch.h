335772eb5ea93fcd21b99a06ecd80b9495ebc5d5
MichaelGlasser <michaelglasser@utexas.edu>
2016-09-29 11:06:27 -0500

    updated

--- Lab 5/Switch.h
+++ Lab 5/Switch.h
@@ -1,1 +1,8 @@
 //
+
+// This will enable PortB 0-2 to serve as
+// the three buttons for the project.
+// PB0 - Play/Pause button
+// PB1 - Stop/Rewind button
+// PB2 - Change mode (instrument change)
+void SwitchesInit(void);
